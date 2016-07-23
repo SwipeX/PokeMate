7444c7039645146e531ac0a8424c87a24aa69b26
Tim Dekker <timdeka@hotmail.com>
2016-07-23 16:49:30 -0400

    small update to UI.

--- src/main/java/dekk/pw/pokemate/PokeMateUI.java
+++ src/main/java/dekk/pw/pokemate/PokeMateUI.java
@@ -2,11 +2,13 @@ package dekk.pw.pokemate;
 
 import com.google.maps.model.LatLng;
 import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MainApp;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.object.*;
 import com.lynden.gmapsfx.shapes.Polygon;
 import com.lynden.gmapsfx.shapes.PolygonOptions;
 import com.pokegoapi.api.map.fort.Pokestop;
+import com.pokegoapi.api.player.PlayerProfile;
 import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 import javafx.application.Application;
@@ -16,6 +18,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEvent;
 import javafx.stage.Stage;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +36,9 @@ public class PokeMateUI extends Application implements MapComponentInitializedLi
     public static final double VARIANCE = .004;
     public static Marker marker;
     public static List<Pokestop> pokestops = new ArrayList<>();
+    int[] requiredXp = new int[]{0, 1000, 3000, 6000, 10000, 15000, 21000, 28000, 36000, 45000, 55000, 65000, 75000,
+            85000, 100000, 120000, 140000, 160000, 185000, 210000, 260000, 335000, 435000, 560000, 710000, 900000, 1100000,
+            1350000, 1650000, 2000000, 2500000, 3000000, 3750000, 4750000, 6000000, 7500000, 9500000, 12000000, 15000000, 20000000};
 
     @Override
     public void start(final Stage stage) throws Exception {
@@ -82,6 +88,7 @@ public class PokeMateUI extends Application implements MapComponentInitializedLi
                 .strokeColor("blue")
                 .strokeWeight(2)
                 .editable(false)
+                .clickable(false)
                 .fillColor("lightBlue")
                 .fillOpacity(0.3);
 
@@ -90,6 +97,12 @@ public class PokeMateUI extends Application implements MapComponentInitializedLi
         //Marker of current player, thread to update a 'hack refresh'
         marker = new Marker(new MarkerOptions().position(center).title("Player"));
         map.addMarker(marker);
+        final InfoWindowOptions infoOptions = new InfoWindowOptions();
+        infoOptions.content("<h3>Loading...</h3>")
+                .position(center);
+
+        InfoWindow window = new InfoWindow(infoOptions);
+        window.open(map, marker);
         new Thread(() -> {
             while (true) {
                 Platform.runLater(() -> {
@@ -100,6 +113,14 @@ public class PokeMateUI extends Application implements MapComponentInitializedLi
                 });
                 try {
                     try {
+                        Platform.runLater(() -> {
+                            PlayerProfile player = context.getApi().getPlayerProfile();
+                            double nextXP = requiredXp[player.getStats().getLevel()] - requiredXp[player.getStats().getLevel() - 1];
+                            double curLevelXP = player.getStats().getExperience() - requiredXp[player.getStats().getLevel() - 1];
+                            String ratio = new DecimalFormat("#0.00").format(curLevelXP / nextXP * 100.D);
+                            window.setContent("<h3>" + player.getUsername() + " (" + player.getStats().getLevel() + ") : " +
+                                    ratio + "% " + player.getStats().getExperience() + " total exp </h3>");
+                        });
                         for (Pokestop ps : context.getApi().getMap().getMapObjects().getPokestops()) {
                             if (!pokestops.contains(ps)) {
                                 Platform.runLater(() -> map.addMarker(new Marker(new MarkerOptions().position(center).title("Pokestop: " + ps.getId()))));

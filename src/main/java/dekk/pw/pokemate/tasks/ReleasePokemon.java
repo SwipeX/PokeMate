0b8442fdcbb955f0cae8e51331c4b8da05b7c403
Tim Dekker <timdeka@hotmail.com>
2016-07-23 23:17:37 -0400

    perfect ratio adjustment

--- src/main/java/dekk/pw/pokemate/tasks/ReleasePokemon.java
+++ src/main/java/dekk/pw/pokemate/tasks/ReleasePokemon.java
@@ -17,13 +17,13 @@ import java.util.stream.Collectors;
  */
 public class ReleasePokemon implements Task {
 
-    private static final int PERFECT_IV = 41;
+    private static final int PERFECT_IV = 85;
 
     public void run(Context context) {
         Map<PokemonIdOuterClass.PokemonId, List<Pokemon>> groups = context.getApi().getInventories().getPokebank().getPokemons().stream().collect(Collectors.groupingBy(Pokemon::getPokemonId));
         for (List<Pokemon> list : groups.values()) {
             Collections.sort(list, (a, b) -> a.getCp() - b.getCp());
-            list.stream().filter(p -> p.getIndividualAttack() + p.getIndividualDefense() + p.getIndividualStamina() < PERFECT_IV).forEach(p -> {
+            list.stream().filter(p -> !p.getFavorite() || getIvRatio(p) < PERFECT_IV).forEach(p -> {
                 //Passing this filter means they are not a 'perfect pokemon'
                 try {
                     System.out.println("Transferring " + (list.indexOf(p) + 1) + "/" + list.size() + " " + p.getPokemonId() + " CP " + p.getCp() + " [" + p.getIndividualStamina() + "/" + p.getIndividualDefense() + "/" + p.getIndividualStamina() + "]");
@@ -33,5 +33,13 @@ public class ReleasePokemon implements Task {
                 }
             });
         }
+    }
+
+    /**
+     * @param pokemon the pokemon for which an IV ratio is desired.
+     * @return an integer 0-100 on the individual value of the pokemon.
+     */
+    public int getIvRatio(Pokemon pokemon) {
+        return (pokemon.getIndividualAttack() + pokemon.getIndividualDefense() + pokemon.getIndividualStamina()) * 100 / 45;
     }
 }

3d2caf5688d8b31fe1bbd621a30c861eef80f7e2
mxc1868 <mxc1868@gmail.com>
2016-07-24 21:03:58 -0400

    new feature added & bugs fix
    New feature:
    1. When not setting the preferred ball, it will follow the priority that
    POKEBALL>GREATBALL>ULTRABALL>MASTERBALL
    2. Showing remaining balls info when catching pokemon
    
    Bug fix:
    CP filter bug

--- src/main/java/dekk/pw/pokemate/tasks/CatchPokemon.java
+++ src/main/java/dekk/pw/pokemate/tasks/CatchPokemon.java
@@ -22,15 +22,64 @@ public class CatchPokemon implements Task {
 
     public void run(Context context) {
         try {
-            Pokeball pokeball;
+            Pokeball pokeball = null;
+            int flag=0;
             List<CatchablePokemon> pokemon = context.getApi().getMap().getCatchablePokemon();
             if (pokemon.size() > 0) {
-                Item ball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(Config.getPreferredBall()));
-                if (ball != null && ball.getCount() > 0) {
-                    pokeball = getBall(Config.getPreferredBall());
-                } else {
-                    pokeball = Pokeball.POKEBALL;
-                }
+            	if (Config.getPreferredBall()==-1)
+            	{
+            		Item item_pokeball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_POKE_BALL_VALUE));
+            		Item item_greatball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_GREAT_BALL_VALUE));
+            		Item item_ultraball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_ULTRA_BALL_VALUE));
+            		Item item_masterball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_MASTER_BALL_VALUE));
+                    if ((item_pokeball != null &&item_greatball != null &&item_ultraball != null && item_masterball!=null)&& (item_pokeball.getCount()+item_greatball.getCount()+item_ultraball.getCount()+item_masterball.getCount()) > 0) {
+                        if (item_pokeball.getCount()>0) {
+                        	pokeball=Pokeball.POKEBALL;
+                        	flag=0;
+                        	System.out.println("Remaining POKEball:"+item_pokeball.getCount());
+                        	System.out.println("Remaining GREATball:"+item_greatball.getCount());
+                        	System.out.println("Remaining ULTRAball:"+item_ultraball.getCount());
+                        	System.out.println("Remaining MASTERball:"+item_masterball.getCount());
+                        }
+                        else if (item_greatball.getCount()>0) {
+                        	pokeball=Pokeball.GREATBALL;
+                        	flag=1;
+                        	System.out.println("Remaining POKEball:"+item_pokeball.getCount());
+                        	System.out.println("Remaining GREATball:"+item_greatball.getCount());
+                        	System.out.println("Remaining ULTRAball:"+item_ultraball.getCount());
+                        	System.out.println("Remaining MASTERball:"+item_masterball.getCount());
+                        }
+                        else if (item_ultraball.getCount()>0) {
+                        	pokeball=Pokeball.ULTRABALL;
+                        	flag=2;
+                        	System.out.println("Remaining POKEball:"+item_pokeball.getCount());
+                        	System.out.println("Remaining GREATball:"+item_greatball.getCount());
+                        	System.out.println("Remaining ULTRAball:"+item_ultraball.getCount());
+                        	System.out.println("Remaining MASTERball:"+item_masterball.getCount());
+                        }
+                        else if (item_masterball.getCount()>0) {
+                        	pokeball=Pokeball.MASTERBALL;
+                        	flag=3;
+                        	System.out.println("Remaining POKEball:"+item_pokeball.getCount());
+                        	System.out.println("Remaining GREATball:"+item_greatball.getCount());
+                        	System.out.println("Remaining ULTRAball:"+item_ultraball.getCount());
+                        	System.out.println("Remaining MASTERball:"+item_masterball.getCount());
+                        }
+                    }
+            	}
+            	else{
+	                Item ball = context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(Config.getPreferredBall()));
+	                if (ball != null && ball.getCount() > 0) {
+	                    pokeball = getBall(Config.getPreferredBall());
+	                    if(pokeball==Pokeball.POKEBALL) flag=0;
+	                    if(pokeball==Pokeball.GREATBALL) flag=1;
+	                    if(pokeball==Pokeball.ULTRABALL) flag=2;
+	                    if(pokeball==Pokeball.MASTERBALL) flag=3;
+	                } else {
+	                    pokeball = Pokeball.POKEBALL;
+	                    flag=0;
+	                }
+            	}
                 CatchablePokemon target = pokemon.get(0);
                 if (target != null) {
                     Walking.setLocation(context);
@@ -38,9 +87,14 @@ public class CatchPokemon implements Task {
                     if (encounterResult.wasSuccessful()) {
                         CatchResult catchResult = target.catchPokemon(pokeball);
                         if (catchResult.getStatus().equals(CatchPokemonResponseOuterClass.CatchPokemonResponse.CatchStatus.CATCH_SUCCESS)) {
-                            System.out.println("Caught a " + target.getPokemonId() + " using a " + ball.getItemId().name());
+                        	Item tempball=null;
+                        	if(flag==0) tempball=context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_POKE_BALL_VALUE));
+                        	if(flag==1) tempball=context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_GREAT_BALL_VALUE));
+                        	if(flag==2) tempball=context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_ULTRA_BALL_VALUE));
+                        	if(flag==3) tempball=context.getApi().getInventories().getItemBag().getItem(ItemIdOuterClass.ItemId.forNumber(ItemIdOuterClass.ItemId.ITEM_MASTER_BALL_VALUE));
+                            System.out.println("Caught a " + target.getPokemonId()+ " using a " + tempball.getItemId().name());
                         } else {
-                            System.out.println(target.getPokemonId() + " fled.");
+                            System.out.println(target.getPokemonId() + " failed.");
                         }
                     }
                 }
--- src/main/java/dekk/pw/pokemate/tasks/TaskController.java
+++ src/main/java/dekk/pw/pokemate/tasks/TaskController.java
@@ -1,34 +1,35 @@
 package dekk.pw.pokemate.tasks;
 
-import com.google.maps.model.LatLng;
-import dekk.pw.pokemate.Context;
-import dekk.pw.pokemate.Config;
-
 import java.util.ArrayList;
+
+import com.google.maps.model.LatLng;
+
+import dekk.pw.pokemate.Config;
+import dekk.pw.pokemate.Context;
 
 /**
  * Created by TimD on 7/21/2016.
  */
 public class TaskController extends Thread {
-    public static final double VARIANCE = .004;
+    public static final double yVARIANCE = Config.getrange_y();
+    public static final double xVARIANCE = Config.getrange_x();
     private Context context;
     private static ArrayList<Task> tasks = new ArrayList<Task>();
 
     public TaskController(Context context) {
         this.context = context;
-        tasks.add(new Navigate(context, new LatLng(context.getLat().get() - VARIANCE, context.getLng().get() - VARIANCE),
-                new LatLng(context.getLat().get() + VARIANCE, context.getLng().get() + VARIANCE)));
+        tasks.add(new Navigate(context, new LatLng(context.getLat().get() - yVARIANCE, context.getLng().get() - xVARIANCE),
+                new LatLng(context.getLat().get() + yVARIANCE, context.getLng().get() + xVARIANCE)));
         tasks.add(new Update());
         tasks.add(new CatchPokemon());
         tasks.add(new ReleasePokemon());
-        if (Config.isEvolvingActive()) {
-            tasks.add(new EvolvePokemon());
-        }
+        if(Config.isEvolvingActive())
+        	tasks.add(new EvolvePokemon());
         tasks.add(new TagPokestop());
         tasks.add(new HatchEgg());
-        if (Config.isDropItems()) {
-            tasks.add(new DropItems());
-        }
+		if(Config.isDropItems()) 
+			tasks.add(new DropItems());
+		
     }
 
     /**

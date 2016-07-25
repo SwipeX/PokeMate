02f8d68de917794c240eec70b9dbb6b3805a3421
Tim Dekker <timdeka@gmail.com>
2016-07-25 14:46:52 -0400

    table fix @html

--- src/main/java/dekk/pw/pokemate/tasks/HatchEgg.java
+++ src/main/java/dekk/pw/pokemate/tasks/HatchEgg.java
@@ -1,9 +1,9 @@
 package dekk.pw.pokemate.tasks;
 
-import com.pokegoapi.api.pokemon.HatchedEgg;
-import com.pokegoapi.api.pokemon.Pokemon;
-import com.pokegoapi.exceptions.LoginFailedException;
-import com.pokegoapi.exceptions.RemoteServerException;
+//import com.pokegoapi.api.pokemon.HatchedEgg;
+//import com.pokegoapi.api.pokemon.Pokemon;
+//import com.pokegoapi.exceptions.LoginFailedException;
+//import com.pokegoapi.exceptions.RemoteServerException;
 import dekk.pw.pokemate.Context;
 
 import java.util.List;
@@ -14,19 +14,20 @@ import java.util.List;
 public class HatchEgg implements Task {
     @Override
     public void run(Context context) {
-        try {
-            List<HatchedEgg> eggs = context.getApi().getInventories().getHatchery().queryHatchedEggs();
-            eggs.forEach(egg -> {
-                Pokemon hatchedPokemon = context.getApi().getInventories().getPokebank().getPokemonById(egg.getId());
-                String details = String.format("candy: %s  exp: %s  stardust: %s", egg.getCandy(), egg.getExperience(), egg.getStardust());
-                if (hatchedPokemon == null) {
-                    System.out.println("Hatched egg " + egg.getId() + " " + details);
-                } else {
-                    System.out.println("Hatched " + hatchedPokemon.getPokemonId() + " with " + hatchedPokemon.getCp() + " CP " + " - " + details);
-                }
-            });
-        } catch (RemoteServerException | LoginFailedException e) {
-            e.printStackTrace();
-        }
+ //       try {
+            //We need to wait until this is in the master branch
+//            List<HatchedEgg> eggs = context.getApi().getInventories().getHatchery().queryHatchedEggs();
+//            eggs.forEach(egg -> {
+//                Pokemon hatchedPokemon = context.getApi().getInventories().getPokebank().getPokemonById(egg.getId());
+//                String details = String.format("candy: %s  exp: %s  stardust: %s", egg.getCandy(), egg.getExperience(), egg.getStardust());
+//                if (hatchedPokemon == null) {
+//                    System.out.println("Hatched egg " + egg.getId() + " " + details);
+//                } else {
+//                    System.out.println("Hatched " + hatchedPokemon.getPokemonId() + " with " + hatchedPokemon.getCp() + " CP " + " - " + details);
+//                }
+//            });
+//        } catch (RemoteServerException | LoginFailedException e) {
+//            e.printStackTrace();
+//        }
     }
 }

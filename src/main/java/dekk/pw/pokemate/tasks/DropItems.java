2c0faf7860be32cd7bfd1d7fa6fe6f0e8b473d4a
Kai <git@holzinger-it.de>
2016-07-26 18:51:30 +0200

    j
    j

--- src/main/java/dekk/pw/pokemate/tasks/DropItems.java
+++ src/main/java/dekk/pw/pokemate/tasks/DropItems.java
@@ -1,25 +1,26 @@
 package dekk.pw.pokemate.tasks;
 
+import POGOProtos.Inventory.Item.ItemIdOuterClass;
 import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 import dekk.pw.pokemate.Context;
-
-import static POGOProtos.Inventory.Item.ItemIdOuterClass.ItemId;
 
 /**
  * Created by TimD on 7/22/2016.
  */
 public class DropItems implements Task {
-    ItemId[] UNWANTED = new ItemId[]{ItemId.ITEM_REVIVE, ItemId.ITEM_MAX_REVIVE,};
+    ItemIdOuterClass.ItemId[] UNWANTED = new ItemIdOuterClass.ItemId[]{ItemIdOuterClass.ItemId.ITEM_POTION,
+            ItemIdOuterClass.ItemId.ITEM_SUPER_POTION, ItemIdOuterClass.ItemId.ITEM_MAX_POTION, ItemIdOuterClass.ItemId.ITEM_RAZZ_BERRY,
+            ItemIdOuterClass.ItemId.ITEM_REVIVE, ItemIdOuterClass.ItemId.ITEM_MAX_REVIVE,};
 
     @Override
     public void run(Context context) {
         try {
-            for (ItemId aUNWANTED : UNWANTED) {
-                int count = context.getApi().getInventories().getItemBag().getItem(aUNWANTED).getCount();
-                context.getApi().getInventories().getItemBag().removeItem(aUNWANTED, count);
-                if (count > 0 ) {
-                    System.out.println("Removed " + count + " " + aUNWANTED);
+            for (ItemIdOuterClass.ItemId id : UNWANTED) {
+                int count = context.getApi().getInventories().getItemBag().getItem(id).getCount();
+                context.getApi().getInventories().getItemBag().removeItem(id, count);
+                if (count > 0) {
+                    System.out.println("Removed " + count + " " + id);
                 }
             }
         } catch (RemoteServerException | LoginFailedException e) {

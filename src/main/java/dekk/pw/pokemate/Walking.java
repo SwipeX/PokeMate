65696bae980f7fc505770af4443554bcacc39a21
Kai <git@holzinger-it.de>
2016-07-27 10:26:07 +0200

    update

--- src/main/java/dekk/pw/pokemate/Walking.java
+++ src/main/java/dekk/pw/pokemate/Walking.java
@@ -12,7 +12,7 @@ import java.util.TimerTask;
  */
 public class Walking {
 
-    private static double getSmallRandom() {
+    public static double getSmallRandom() {
         return Math.random() * 0.0001 - 0.00005;
     }
 
@@ -20,7 +20,7 @@ public class Walking {
         setLocation(false, context);
     }
 
-    private static void setLocation(boolean random, Context context) {
+    public static void setLocation(boolean random, Context context) {
         if (random) {
             context.getApi().setLocation(context.getLat().get() + getSmallRandom(), context.getLng().get() + getSmallRandom(), 0);
         } else {
@@ -28,36 +28,34 @@ public class Walking {
         }
     }
 
-
-        public static void walk(S2LatLng end, final Context context) {
-            if (context.isWalking()) return;
-            context.getWalking().set(true);
-            S2LatLng start = S2LatLng.fromDegrees(context.getLat().get(), context.getLng().get());
-            S2LatLng diff = end.sub(start);
-            double distance = start.getEarthDistance(end);
-            long timeout = 200L;
-            double timeRequired = distance / Config.getSpeed();
-            final AtomicDouble stepsRequired = new AtomicDouble(timeRequired / (timeout / 1000D));
-            double deltaLat = diff.latDegrees() / stepsRequired.get();
-            double deltaLng = diff.lngDegrees() / stepsRequired.get();
-            //Schedule a timer to walk every 200 ms
-            new Timer().scheduleAtFixedRate(new TimerTask() {
-                @Override
-                public void run() {
-                    context.getLat().addAndGet(deltaLat);
-                    context.getLng().addAndGet(deltaLng);
-                    stepsRequired.getAndAdd(-1);
-                    if (stepsRequired.get() <= 0) {
-                        System.out.println("Destination reached.");
-                        context.getWalking().set(false);
-                        cancel();
-
-                    }
-                    //   System.out.println(context.getLat().get() + " " + context.getLng().get() + " " + stepsRequired);
+    public static void walk(S2LatLng end, final Context context) {
+        if (context.isWalking())
+            return;
+        context.getWalking().set(true);
+        S2LatLng start = S2LatLng.fromDegrees(context.getLat().get(), context.getLng().get());
+        S2LatLng diff = end.sub(start);
+        double distance = start.getEarthDistance(end);
+        long timeout = 200L;
+        double timeRequired = distance / Config.getSpeed();
+        final AtomicDouble stepsRequired = new AtomicDouble(timeRequired / (timeout / 1000D));
+        double deltaLat = diff.latDegrees() / stepsRequired.get();
+        double deltaLng = diff.lngDegrees() / stepsRequired.get();
+        //Schedule a timer to walk every 200 ms
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                context.getLat().addAndGet(deltaLat);
+                context.getLng().addAndGet(deltaLng);
+                stepsRequired.getAndAdd(-1);
+                if (stepsRequired.get() <= 0) {
+                    System.out.println("Destination reached.");
+                    context.getWalking().set(false);
+                    cancel();
                 }
-            }, 0, timeout);
-        }
-
+                //   System.out.println(context.getLat().get() + " " + context.getLng().get() + " " + stepsRequired);
+            }
+        }, 0, timeout);
+    }
 
     public static void walk(Context context, DirectionsStep[] steps) {
         new Thread(() -> {
@@ -88,7 +86,7 @@ public class Walking {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                         //  Log.i("WALKER", "Set location: [" + context.getLat().get() + ", " + context.getLng().get() + "]");
+                        //  Log.i("WALKER", "Set location: [" + context.getLat().get() + ", " + context.getLng().get() + "]");
                         remainingSteps--;
                     }
                     //Log.i("WALKER", "Arrived at: [" + step.endLocation.lat + ", " + step.endLocation.lng + "]");

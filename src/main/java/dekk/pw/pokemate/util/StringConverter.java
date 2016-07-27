462dbf7b10ae28ffea0332312df953f289b02277
Kai <git@holzinger-it.de>
2016-07-27 10:37:18 +0200

    some comments

--- src/main/java/dekk/pw/pokemate/util/StringConverter.java
+++ src/main/java/dekk/pw/pokemate/util/StringConverter.java
@@ -5,10 +5,12 @@ package dekk.pw.pokemate.util;
  */
 public class StringConverter {
 
+    //return a pokemon name thats not all uppercase
     public static String convertPokename(String pokeName){
         return pokeName.substring(0, 1).toUpperCase() + pokeName.substring(1).toLowerCase();
     }
 
+    //return an item name thats not all uppercase and doesn't have underscores
     public static String convertItem(String itemName){
         String[] result = (itemName.substring(5,6).toUpperCase() + itemName.substring(6).replaceAll("_", " ").toLowerCase()).split("\\s+");
         return result[0] + " " + result[1].substring(0,1).toUpperCase() + result[1].substring(1);

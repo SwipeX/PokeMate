2639130276bfe8553e14d94548ffafdb3f8af52e
gsplash <gsplashed@gmail.com>
2016-08-03 11:32:40 +0300

    Fixing drop items compiling issues

--- src/main/java/dekk/pw/pokemate/tasks/DropItems.java
+++ src/main/java/dekk/pw/pokemate/tasks/DropItems.java
@@ -24,22 +24,17 @@ class DropItems extends Task implements Runnable {
 
     @Override
     public void run() {
-        try {
-            Config.getDroppedItems().forEach( (itemToDrop, minAmount) -> {
-                ItemId id = ItemId.valueOf(itemToDrop);
-                try {
-                    Time.sleepRate();
-                    int countToDrop = context.getApi().getInventories().getItemBag().getItem(id).getCount() - minAmount;
-                    Time.sleepRate();
-                    if (countToDrop > 0) {
-                        context.getApi().getInventories().getItemBag().removeItem(id, countToDrop);
-                        String removedItem = "Removed " + StringConverter.titleCase(id.name()) + "(x" + countToDrop + ")";
-                        PokeMateUI.toast(removedItem, "Items removed!", "icons/items/" + id.getNumber() + ".png");
-                        context.setConsoleString("DropItems", removedItem);
-                    }
-                } catch (RemoteServerException | LoginFailedException e) {
-                    context.setConsoleString("Debug", "[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] - " + context.getConsoleStrings().get("Debug") + "    [DropItems] Exceeded Rate Limit\n");
-                    e.printStackTrace();
+        Config.getDroppedItems().forEach((itemToDrop, minAmount) -> {
+            ItemId id = ItemId.valueOf(itemToDrop);
+            try {
+                Time.sleepRate();
+                int countToDrop = context.getApi().getInventories().getItemBag().getItem(id).getCount() - minAmount;
+                Time.sleepRate();
+                if (countToDrop > 0) {
+                    context.getApi().getInventories().getItemBag().removeItem(id, countToDrop);
+                    String removedItem = "Removed " + StringConverter.titleCase(id.name()) + "(x" + countToDrop + ")";
+                    PokeMateUI.toast(removedItem, "Items removed!", "icons/items/" + id.getNumber() + ".png");
+                    context.setConsoleString("DropItems", removedItem);
                 }
             } catch (RemoteServerException | LoginFailedException e) {
                 context.setConsoleString("Debug", "[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] - " + context.getConsoleStrings().get("Debug") + "    [DropItems] Exceeded Rate Limit\n");
